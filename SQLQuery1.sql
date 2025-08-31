4c69fbf614748bbdf6cde23533784d81f22697da
Gold Noor <noordahbh@gmail.com>
2025-08-31 19:01:50 +0300

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,6 +0,0 @@
--- Explore All Objects in The Database
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-
--- Explore All Columns in Gold Schema
-SELECT * FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'gold'
